1023b8078246131013ef54d093a172a4c03bf0b3
JustVasiliy <vasilyrd2001@gmail.com>
2021-12-10 11:09:51 +0200

    made todolist with classes

--- scriptClass.js
+++ scriptClass.js
@@ -9,6 +9,7 @@ const section = document.createElement('section');
 const h1 = document.createElement('h1');
 h1.innerText = 'Todo List';
 const listItems = document.createElement('ul');
+listItems.id = 'ul'
 const divCreateNewItem = document.createElement('div');
 divCreateNewItem.classList.add('createNewItem');
 const inputCreateName = document.createElement('input');
@@ -24,7 +25,7 @@ section.appendChild(divCreateNewItem);
 divCreateNewItem.appendChild(inputCreateName);
 divCreateNewItem.appendChild(btnCreate);
 
-
+console.log(listItems)
 
 //class for create todo-object
 class FormCreateTodo {
@@ -44,7 +45,10 @@ class TodoList {
         this.array = array;
     }
     render() {
-        listItems.innerHTML = '';
+        const nodeUl = document.getElementById('ul');
+        while (nodeUl.firstChild) nodeUl.removeChild(nodeUl.firstChild);
+        
+        
         this.array.arrayItems.forEach(element => {
             const newItems = document.createElement('li');
             newItems.classList.add('parentPosition')
@@ -132,24 +136,95 @@ class Store {
 
         }
     }
-    delete() {
-
+    delete(event) {
+        btnDelete= document.querySelectorAll(".delete");
+        let myButtonArr = Array.from(btnDelete);
+        let filtArrBtn = myButtonArr.filter(item => {
+            if(item === event.target){
+                return true;
+            }
+        });
+        
+        for(let i =0; i<this.arrayItems.length; i++){
+            if(this.arrayItems[i].id === +filtArrBtn[0].parentElement.parentElement.getAttribute('id')){
+                // itemsArray.splice(i,1);
+                this.arrayItems[i].deleted = true;
+                
+            }
+                
+        }
     }
-    change() {
+    change(event) {
+        btnChange= document.querySelectorAll(".change");
+        let myButtonArrChange = Array.from(btnChange);
+        let filtArrBtnChange = myButtonArrChange.filter(item => {
+            if(item === event.target){
+                return true;
+            }
+        });
+        //Создание кнопки save
+        const save = document.createElement('button');
+        save.classList.add('saveBtn')
+        
+        save.innerText = 'save';
+        
+        filtArrBtnChange[0].classList.add('parentPosition')
+        filtArrBtnChange[0].appendChild(save);
+        for(let i =0; i<this.arrayItems.length; i++){
+            if(this.arrayItems[i].id === +filtArrBtnChange[0].parentElement.parentElement.getAttribute('id')){
+               
+                
+                const inputChange = document.createElement('input');
+                inputChange.type = 'text';
+                inputChange.classList.add('inputChange');
+                
+                inputChange.setAttribute('maxlength', '50')
+                inputChange.value = `${this.arrayItems[i].name}`;
 
+                filtArrBtnChange[0].parentElement.parentElement.appendChild(inputChange);
+                let testArray = this.arrayItems;
+                //функция кнопки save 
+                save.onclick = function(){
+                    console.log(testArray)
+                    if(inputChange.value.trim() !== ''){
+                       
+                        testArray[i].name = inputChange.value.trim();
+                         
+                        inputChange.classList.add('deleted');
+            
+                        listItems.innerHTML = '';
+                                    
+                        testArray.forEach(elem=>{
+                        const newItems = document.createElement('li');
+                       
+                        new TodoList(store).render();
+                                        
+                        });
+                    }
+                    
+                    
+                }
+                
+               
+            }
+                
+        }
     }
 }
 
 
 
 let store = new Store(new FormCreateTodo('first'))
-
+new TodoList(store).render();
 
 btnCreate.onclick = function () {
+    if(inputCreateName.value.trim() !== ""){
+        store.saveItem(new FormCreateTodo(inputCreateName.value));
 
-    store.saveItem(new FormCreateTodo(inputCreateName.value));
+        new TodoList(store).render();
+    }
 
-    new TodoList(store).render();
+    inputCreateName.value = ''
 }
 
 //listeners
@@ -157,15 +232,18 @@ listItems.onclick = function (event) {
     if (event.target.className === 'check') {
         store.check(event);
         new TodoList(store).render();
-        console.log(store)
+      
 
 
     } else if (event.target.className === 'delete') {
-
-
+        store.delete(event);
+        new TodoList(store).render();
+       
     }
     else if (event.target.className === 'change') {
-
+        store.change(event);
+        
+        console.log(store)
     }
 }
 
